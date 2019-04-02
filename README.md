4d2270fdd3b40b90f2e3163a9387e1d3e55eaff3
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-04-02 18:18:52 +1300

    notes

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Xelmish
 
 XNA + Elmish
+
+TODO:
+
+- config object:
+  - specify clear colour
+  - specify if mouse visible
+  - fixed framerate, possibly
+  - fps in console?
+- how would renderView work?
+  - remove from loop class, this time
+  - needs a way to access mouse/keyboard state
+    - a cache of pressed, released?
+  - needs a general 'interaction checker', that ties in with dispatcher
--- src/GameLoop.fs
+++ src/GameLoop.fs
@@ -2,13 +2,25 @@
 
  open Microsoft.Xna.Framework
  open Microsoft.Xna.Framework.Graphics
+ open Microsoft.Xna.Framework.Input
  
  type GameLoop<'view>() as this = 
         inherit Game()
 
         let graphics = new GraphicsDeviceManager(this)
         let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
         let mutable view: 'view option = None
+        let mutable keyboardState = Unchecked.defaultof<KeyboardState>
+        let mutable mouseState = Unchecked.defaultof<MouseState>
+
+        do 
+            this.IsMouseVisible <- true
+            graphics.SynchronizeWithVerticalRetrace <- true
+            this.IsFixedTimeStep <- false 
+
+        let renderView gameTime viewElements =
+            ()
 
         member __.View
             with set value = 
@@ -17,8 +29,17 @@
         override __.LoadContent() = 
             spriteBatch <- new SpriteBatch(this.GraphicsDevice)
 
-        override __.Update(gameTime) =
-            ()
+        override __.Update _ =
+            keyboardState <- Keyboard.GetState()
+            mouseState <- Mouse.GetState()
 
-        override __.Draw(gameTime) =
+        override __.Draw gameTime =
+
             this.GraphicsDevice.Clear Color.CornflowerBlue
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp)
+
+            match view with
+            | None -> __.Exit ()
+            | Some v -> renderView gameTime v
+
+            spriteBatch.End()
