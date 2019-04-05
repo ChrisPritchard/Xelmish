af22987915318c8a0a61e3b3bae1330d615ca468
Christopher <chrispritchard.nz@gmail.com>
2019-04-05 15:04:52 +1300

    new work on nested types

--- src/Viewables.fs
+++ src/Viewables.fs
@@ -2,13 +2,29 @@
 
 open Model
 
-type Viewable = 
-    | Colour of colour: Colour * rect: Rectangle * children: Viewable
-    | Image of texture: string * colour: Colour * rect: Rectangle * children: Viewable
-    | Text of font: string * text: string * colour: Colour * rect: Rectangle
-    | Clickable of onClick: (unit -> unit) * rect: Rectangle * children: Viewable
+type Viewable = { kind: ViewableType; rect: Rectangle; children: Viewable list }
+and ViewableType =
+    | Colour of colour: Colour
+    | Image of texture: string * colour: Colour
+    | Text of font: string * text: string * colour: Colour
+    | Clickable of onClick: (unit -> unit)
+
+let bs = 2 // possibly have a mutable style map?
     
-let backgroundColour = rgb 150uy 150uy 150uy
+let button onClick (text: string) =
+    fun x y font fc bc drawState ->
+        let spriteFont = drawState.fonts.[font]
+        let textSize = spriteFont.MeasureString (text)
+        let innerText = { 
+            kind = Text (font, text, fc)
+            rect = rect (x + bs) (y + bs) (int textSize.X) (int textSize.Y)
+            children = []
+        }
+        { 
+            kind = Colour bc
+            rect = rect x y (int textSize.X + bs*2) (int textSize.Y + bs*2)
+            children = [ innerText ] 
+        }
 
 let renderView spriteBatch gameTime gameState view =
     
