ea20f82e8a778419791fe5b6fc86eaf91aed7417
Christopher <chrispritchard.nz@gmail.com>
2019-04-05 14:27:49 +1300

    further implementation experimentation

--- src/Viewables.fs
+++ src/Viewables.fs
@@ -13,7 +13,20 @@ type Renderable =
 let button attr contents =
     Colour (rgb 200uy 200uy 200uy)
 
+type Attribute = int -> int -> string option -> int * int * string option
+let x nx: Attribute = fun _ y font -> nx, y, font
+let y ny: Attribute = fun x _ font -> x, ny, font
+let font nfont: Attribute = fun x y _ -> x, y, Some nfont
+
 let renderView spriteBatch gameTime gameState view =
+    
+    let rec processItems sx sy sf items =
+        let mutable x, y, font = sx, sy, sf
+        for item in items do
+            ()
+
+    processItems 0 0 None view
+        
     // for each item, derive new position
     // position can be overriden by explicit x, y options
 
