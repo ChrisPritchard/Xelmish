58d614e766770a845c3d45ff28a284c58a809afa
Christopher <chrispritchard.nz@gmail.com>
2019-04-05 14:16:42 +1300

    ideas around how view would be converted

--- src/Viewables.fs
+++ src/Viewables.fs
@@ -5,41 +5,17 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
 
-type Viewable =
-| Position of x: int * y: int * width: int * height: int * Viewable
-| Window of x: int * y: int * width: int * height: int * Viewable list
-| Row of Viewable list
-| Text of font: string * text: string
-| Button of font: string * text: string * onClick: (unit -> unit)
+type Renderable =
+| Text of x:int * y:int * font:string * fontSize:float * content:string
+| Texture of x:int * y:int * w:int * h:int * Texture2D
+| Colour of x:int * y:int * w:int * h:int * Colour
 
-//let private buttonBack: Texture2D = null
+let button attr contents =
+    Colour (rgb 200uy 200uy 200uy)
 
-let private vector2 x y = Vector2(float32 x, float32 y)
-//let private rectangle x y w h = Rectangle(x, y, w, h)
-let private isInside tx ty tw th x y = x >= tx && x <= tx + tw && y >= ty && y <= ty + th
+let renderView spriteBatch gameTime gameState view =
+    // for each item, derive new position
+    // position can be overriden by explicit x, y options
 
-let rec internal renderViewable (spriteBatch: SpriteBatch) gameTime gameState (px, py, pw, ph) viewable =
-    match viewable with
-    | Position (x, y, w, h, sv) ->
-        renderViewable spriteBatch gameTime gameState (px + x, py + y, w, h) sv
-    | Window (x, y, w, h, svl) ->
-        let div = h / svl.Length
-        svl 
-        |> List.iteri (fun i -> 
-            renderViewable spriteBatch gameTime gameState (px + x, py + y + (i * div), w, div))
-    | Row svl ->
-        let div = pw / svl.Length
-        svl 
-        |> List.iteri (fun i -> 
-            renderViewable spriteBatch gameTime gameState (px + (i * div), py, div, ph))
-    | Text (f, s) ->
-        let font = Map.find f gameState.fonts
-        spriteBatch.DrawString(font, s, vector2 px py, Color.White)
-    | Button (f, s, evt) ->
-        //drawState.spriteBatch.Draw(buttonBack, rectangle px py pw ph, Color.White)
-        let font = Map.find f gameState.fonts
-        spriteBatch.DrawString(font, s, vector2 px py, Color.White)
-        if (gameState.mouseState.X, gameState.mouseState.Y) ||> isInside px py pw ph then
-            if gameState.mouseState.LeftButton = ButtonState.Pressed 
-            && gameState.lastMouseState.LeftButton <> ButtonState.Pressed then
-                evt ()
+    // [x y w h] becomes set x x; set y y etc. then contents gets rendered at that position. x y then gets appended by parent
+    // set x; set y; render; add w to x if row else add h to y if column
