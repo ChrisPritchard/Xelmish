8d20f376885c6abebc86492b97a07df8d11dc222
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-04-08 15:34:05 +1200

    initial concept of a layout

--- src/Viewables.fs
+++ src/Viewables.fs
@@ -10,34 +10,25 @@ type Viewable =
 | Image of key: string * colour: Colour * size: (int * int) * pos: (int * int)
 | Text of text: string * font: string * size: float * colour: Colour * origin: (float * float) * pos: (int * int)
 | Clickable of event: (Unit -> Unit) * size: (int * int) * pos: (int * int)
-
-// Clickable? MouseEvent? KeyboardEvent?
+| Layout of rowDefinitions: Definition list * colDefinitions: Definition list * cells: Cell list * size: (int * int) * pos: (int * int)
+and Definition = Exact of int | Min of int | Percent of float
+and Cell = 
+    | TextCell of row: int * col: int * content: ((int * int) -> Viewable)
+    | RectCell of row: int * col: int * content: ((int * int) -> (int * int) -> Viewable)
 
 let colour colour size pos = Colour (colour, size, pos)
 let image key colour size pos = Image (key, colour, size, pos)
 let text font size colour origin text pos = Text (text, font, size, colour, origin, pos)
 let clickable event size pos = Clickable (event, size, pos)
-
-let stack height (children: ((int * int) -> Viewable list) list) (x, y) =
-    let div = height / children.Length
-    children 
-    |> List.mapi (fun i child ->
-        let pos = (x, y + i * div)
-        child pos)
-    |> List.collect id
-
-let row width (children: ((int * int) -> Viewable list) list) (x, y) =
-    let div = width / children.Length
-    children 
-    |> List.mapi (fun i child ->
-        let pos = (x + i * div, y)
-        child pos)
-    |> List.collect id
+let layout rows cols cells size pos = Layout (rows, cols, cells, size, pos)
 
 let private vector2 x y = Vector2(float32 x, float32 y)
 let private isInside tx ty tw th x y = x >= tx && x <= tx + tw && y >= ty && y <= ty + th
 
-let internal renderViewable (spriteBatch: SpriteBatch) gameState viewable =
+let private viewablesFrom rows cols cells size pos =
+    []
+
+let rec internal renderViewable (spriteBatch: SpriteBatch) gameState viewable =
     match viewable with
     | Colour (colour, (width, height), (x, y)) ->
         spriteBatch.Draw(gameState.whiteTexture, xnaRect x y width height, xnaColor colour)
@@ -55,3 +46,6 @@ let internal renderViewable (spriteBatch: SpriteBatch) gameState viewable =
             if gameState.mouseState.LeftButton = ButtonState.Pressed 
             && gameState.lastMouseState.LeftButton <> ButtonState.Pressed then
                 event ()
+    | Layout (rows, cols, cells, size, pos) ->
+        viewablesFrom rows cols cells size pos
+        |> List.iter (renderViewable (spriteBatch: SpriteBatch) gameState)
