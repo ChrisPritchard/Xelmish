185f7f5117094da47fc0f0d7c56856faef82da8c
Christopher <chrispritchard.nz@gmail.com>
2019-04-09 08:21:25 +1200

    reverted to old model. i am overthinking this. let layout be the responsibility of the client application

--- samples/single-counter/Program.fs
+++ samples/single-counter/Program.fs
@@ -26,22 +26,22 @@ let update msg m =
 let view model dispatch =
 
     let text = text "connection"
-    let button s event (width, height) (x, y) = [
-        colour Colours.blue (width, height) (x, y)
-        text 16. Colours.white (-0.5, -0.5) s (x + width/2, y+height/2)
-        clickable event (width, height) (x, y)
-    ]
+    let button s event (x, y) = 
+        let width, height = 100, 30 
+        [
+            colour Colours.blue (width, height) (x, y)
+            text 20. Colours.white (-0.5, -0.5) s (x + width/2, y+height/2)
+            clickable event (width, height) (x, y)
+        ]
 
     [
-        text 30. Colours.black (0., 0.) (sprintf "Counter value: %i" model.Count) (100, 60)
-        layout [ Even; Even; Even; Even ] [ Even; Even; ] [
-            listCell 0 0 (button "- counter" (fun () -> dispatch Decrement))
-            listCell 0 1 (button "+ counter" (fun () -> dispatch Increment))
-            textCell 1 0 (text 20. Colours.black (0., 0.) (sprintf "Step size: %i" model.StepSize))
-            listCell 2 0 (button "- step size" (fun () -> dispatch (SetStepSize (model.StepSize - 1))))
-            listCell 2 1 (button "+ step size" (fun () -> dispatch (SetStepSize (model.StepSize + 1))))
-            listCell 3 0 (button "reset" (fun () -> dispatch Reset))
-        ] (220, 100) (100, 100)
+        yield text 30. Colours.black (0., 0.) (sprintf "Counter value: %i" model.Count) (100, 60)
+        yield! button "- counter" (fun () -> dispatch Decrement) (100, 100)
+        yield! button "+ counter" (fun () -> dispatch Increment) (220, 100)
+        yield text 20. Colours.black (0., 0.) (sprintf "Step size: %i" model.StepSize) (100, 140)
+        yield! button "- step size" (fun () -> dispatch (SetStepSize (model.StepSize - 1))) (100, 180)
+        yield! button "+ step size" (fun () -> dispatch (SetStepSize (model.StepSize + 1))) (220, 180)
+        yield! button "reset" (fun () -> dispatch Reset) (100, 220)
     ]
 
 [<EntryPoint>]
--- src/Viewables.fs
+++ src/Viewables.fs
@@ -10,64 +10,14 @@ type Viewable =
 | Image of key: string * colour: Colour * size: (int * int) * pos: (int * int)
 | Text of text: string * font: string * size: float * colour: Colour * origin: (float * float) * pos: (int * int)
 | Clickable of event: (Unit -> Unit) * size: (int * int) * pos: (int * int)
-| Layout of rowDefinitions: Definition list * colDefinitions: Definition list * cells: Cell list * size: (int * int) * pos: (int * int)
-and Definition = Exact of int | Percent of float | Even
-and Cell = 
-    | TextCell of row: int * col: int * content: ((int * int) -> Viewable)
-    | RectCell of row: int * col: int * content: ((int * int) -> (int * int) -> Viewable)
-    | ListCell of row: int * col: int * content: ((int * int) -> (int * int) -> Viewable list) 
 
 let colour colour size pos = Colour (colour, size, pos)
 let image key colour size pos = Image (key, colour, size, pos)
 let text font size colour origin text pos = Text (text, font, size, colour, origin, pos)
 let clickable event size pos = Clickable (event, size, pos)
-let layout rows cols cells size pos = Layout (rows, cols, cells, size, pos)
-let textCell row col content = TextCell (row, col, content)
-let rectCell row col content = RectCell (row, col, content)
-let listCell row col content = ListCell (row, col, content)
 
 let private vector2 x y = Vector2(float32 x, float32 y)
 let private isInside tx ty tw th x y = x >= tx && x <= tx + tw && y >= ty && y <= ty + th
-
-let private sizesFor maxSize definitions =
-    let sizes = Array.create (List.length definitions) 0
-    let folder (rest, sizeLeft) (i, item) =
-        match item with
-        | Even -> (i, item)::rest, sizeLeft
-        | Exact s -> sizes.[i] <- s; rest, sizeLeft - s
-        | Percent p -> 
-            let calc = int (p * float maxSize)
-            sizes.[i] <- calc; rest, sizeLeft - calc
-    let remainder, sizeLeft = 
-        Seq.indexed definitions 
-        |> Seq.fold folder ([], maxSize)
-    let divided = sizeLeft / remainder.Length
-    remainder |> Seq.iter (fun (i, _) -> sizes.[i] <- divided)
-    sizes
-
-let private viewablesFrom rows cols cells (width, height) (x, y) =
-    let rowSizes = sizesFor height rows
-    let colSizes = sizesFor width cols
-    let rowStarts = fst (Array.mapFold (fun acc n -> acc, acc + n) y rowSizes)
-    let colStarts = fst (Array.mapFold (fun acc n -> acc, acc + n) x colSizes)
-
-    let bounded r c = 
-        (if r < 0 || r >= rowSizes.Length then 0 else r),
-        (if c < 0 || c >= colSizes.Length then 0 else c)
-
-    let cellMapper = 
-        function
-        | TextCell (r, c, v) ->
-            let r, c = bounded r c
-            [v (colStarts.[c], rowStarts.[r])]
-        | RectCell (r, c, v) -> 
-            let r, c = bounded r c
-            [v (colSizes.[c], rowSizes.[r]) (colStarts.[c], rowStarts.[r])]
-        | ListCell (r, c, v) ->
-            let r, c = bounded r c
-            v (colSizes.[c], rowSizes.[r]) (colStarts.[c], rowStarts.[r])
-
-    cells |> List.collect cellMapper
 
 let rec internal renderViewable (spriteBatch: SpriteBatch) gameState viewable =
     match viewable with
@@ -87,6 +37,3 @@ let rec internal renderViewable (spriteBatch: SpriteBatch) gameState viewable =
             if gameState.mouseState.LeftButton = ButtonState.Pressed 
             && gameState.lastMouseState.LeftButton <> ButtonState.Pressed then
                 event ()
-    | Layout (rows, cols, cells, size, pos) ->
-        viewablesFrom rows cols cells size pos
-        |> List.iter (renderViewable (spriteBatch: SpriteBatch) gameState)
