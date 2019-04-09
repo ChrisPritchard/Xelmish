ac159752b42d4edf701266cb684f6d9239750669
Chris Pritchard <chris.pritchard@edpay.nz>
2019-04-10 11:58:12 +1200

    moved model to program (for now) and sketched out messages

--- samples/tetris-clone/Program.fs
+++ samples/tetris-clone/Program.fs
@@ -1,5 +1,25 @@
 ﻿
 open System
+open Xelmish.Model
+
+type Model =
+| StartPage
+| Playing of PlayingState
+| GameOver of score:int
+| Quit
+and PlayingState = {
+    grid: Colour[,]
+    block: (int * int) list * Colour
+    score: int
+}
+
+type Message = 
+| Tick
+| Drop
+| Left
+| Right
+| Rotate
+| RemoveLine
 
 [<EntryPoint>]
 let main argv =
