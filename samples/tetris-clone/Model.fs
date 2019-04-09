72d72c4be049d49d70d74ed92de4536d40c44f30
Chris Pritchard <chris.pritchard@edpay.nz>
2019-04-10 11:56:29 +1200

    sketch of initial tetris model

---
+++ samples/tetris-clone/Model.fs
@@ -0,0 +1,14 @@
+﻿module Model
+
+open Xelmish.Model
+
+type GameModel =
+| StartPage
+| Playing of PlayingState
+| GameOver of score:int
+| Quit
+and PlayingState = {
+    grid: Colour[,]
+    block: (int * int) list * Colour
+    score: int
+}
