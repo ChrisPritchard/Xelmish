38ae95cc2210d3e63f9205ea2bf2d72f94ada5ff
Christopher <chrispritchard.nz@gmail.com>
2019-04-11 09:44:01 +1200

    fixed up L and J shapes

--- samples/tetris-clone/Playing.fs
+++ samples/tetris-clone/Playing.fs
@@ -22,24 +22,28 @@ and Shape = {
 }
 
 let shapes = [
-    {   rotations = [|  [0,0; 1,0; 0,1; 1,1] |]
+    {   rotations = [|  [0,0; 1,0; 0,1; 1,1] |] // O
         colour = Colours.cyan }
-    {   rotations = [|  [0,0; 1,0; 2,0; 3,0]
+    {   rotations = [|  [0,0; 1,0; 2,0; 3,0]    // I
                         [2,0; 2,1; 2,2; 2,3] |]
         colour = Colours.red }
-    {   rotations = [|  [0,0; 1,0; 1,1; 2,1]
+    {   rotations = [|  [0,0; 1,0; 1,1; 2,1]    // Z
                         [2,0; 2,1; 1,1; 1,2] |] 
         colour = Colours.green }
-    {   rotations = [|  [1,0; 2,0; 1,1; 0,1]
+    {   rotations = [|  [1,0; 2,0; 1,1; 0,1]    // S
                         [1,0; 1,1; 2,1; 2,2] |] 
         colour = Colours.blue }
-    {   rotations = [|  [0,0; 1,0; 2,0; 0,1]
+    {   rotations = [|  [0,0; 1,0; 2,0; 0,1]    // L
+                        [0,0; 1,0; 1,1; 1,2]
+                        [0,1; 1,1; 2,1; 2,0]
                         [1,0; 1,1; 1,2; 2,2] |] 
-        colour = Colours.yellow }
-    {   rotations = [|  [0,0; 1,0; 2,0; 2,1]
-                        [1,0; 1,1; 1,2; 2,0] |] 
+        colour = Colours.orange }
+    {   rotations = [|  [0,0; 1,0; 2,0; 2,1]    // J
+                        [1,0; 1,1; 1,2; 0,2]
+                        [0,0; 0,1; 1,1; 2,1]
+                        [1,0; 2,0; 1,1; 1,2] |] 
         colour = Colours.magenta }
-    {   rotations = [|  [0,0; 1,0; 2,0; 1,1]
+    {   rotations = [|  [0,0; 1,0; 2,0; 1,1]    // T
                         [1,0; 1,1; 1,2; 0,1]
                         [0,1; 1,1; 2,1; 1,0]
                         [1,0; 1,1; 1,2; 2,1] |]
