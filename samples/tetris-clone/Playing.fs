8d4511251709b2e49ad2eac972d0057b082abefc
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-04-10 20:15:30 +1200

    implemented move and rotate properly

---
+++ samples/tetris-clone/Playing.fs
@@ -0,0 +1,104 @@
+﻿module Playing
+
+open Xelmish.Model
+
+let gridWidth = 10
+let gridHeight = 20
+let startPos = (gridWidth / 2, 0)
+let random = System.Random ()
+
+type Model = {
+    staticBlocks: Map<int * int, Colour>
+    blockPosition: (int * int)
+    shapeType: Shape
+    rotationIndex: int
+    score: int
+}
+and Shape = {
+    rotations: ((int * int) list) []
+    colour: Colour
+}
+
+let shapes = [
+    {   rotations = [|  [0,0; 1,0; 0,1; 1,1] |]
+        colour = Colours.cyan }
+    {   rotations = [|  [0,0; 1,0; 2,0; 3,0]
+                        [2,0; 2,1; 2,2; 2,3] |]
+        colour = Colours.red }
+    {   rotations = [|  [0,0; 1,0; 1,1; 2,1]
+                        [2,0; 2,1; 1,1; 1,2] |] 
+        colour = Colours.green }
+    {   rotations = [|  [1,0; 2,0; 1,1; 0,1]
+                        [1,0; 1,1; 2,1; 2,2] |] 
+        colour = Colours.blue }
+    {   rotations = [|  [0,0; 1,0; 2,0; 0,1]
+                        [1,0; 1,1; 1,2; 2,2] |] 
+        colour = Colours.yellow }
+    {   rotations = [|  [0,0; 1,0; 2,0; 2,1]
+                        [1,0; 1,1; 1,2; 2,0] |] 
+        colour = Colours.magenta }
+    {   rotations = [|  [0,0; 1,0; 2,0; 1,1]
+                        [1,0; 1,1; 1,2; 0,1]
+                        [0,1; 1,1; 2,1; 1,0]
+                        [1,0; 1,1; 1,2; 2,1] |]
+        colour = Colours.silver }
+]
+
+let init () = {
+        staticBlocks = Map.empty
+        blockPosition = startPos
+        shapeType = shapes.[random.Next(shapes.Length)]
+        rotationIndex = 0
+        score = 0
+    }
+
+type Message = 
+| Tick
+| Drop
+| Left
+| Right
+| Rotate
+
+let tilesFor model =
+    let x, y = model.blockPosition
+    model.shapeType.rotations.[model.rotationIndex] 
+    |> List.map (fun (dx, dy) -> x + dx, y + dy)
+
+let outOfBounds (x, y) =
+    x < 0 || x >= gridWidth || y < 0 || y >= gridHeight
+
+let moveShape (dx, dy) model =
+    let newModel = { model with blockPosition = let (x, y) = model.blockPosition in x + dx, y + dy }
+    let newTiles = tilesFor newModel
+
+    if List.exists outOfBounds newTiles then model
+    elif List.exists (fun tile -> Map.containsKey tile model.staticBlocks) newTiles then
+        let oldTiles = tilesFor model
+        let newStatics = 
+            (model.staticBlocks, oldTiles) 
+            ||> List.fold (fun statics tile -> 
+                Map.add tile model.shapeType.colour statics)
+        { model with
+            staticBlocks = newStatics
+            shapeType = shapes.[random.Next(shapes.Length)]
+            rotationIndex = 0
+            blockPosition = startPos } // TODO: check for game over
+    else
+        newModel
+
+let rotateShape model =
+    let newModel = { model with rotationIndex = (model.rotationIndex + 1) % model.shapeType.rotations.Length }
+    let newTiles = tilesFor newModel
+
+    if  List.exists outOfBounds newTiles 
+        || List.exists (fun tile -> Map.containsKey tile model.staticBlocks) newTiles then
+        model
+    else
+        newModel
+
+let update message model =
+    match message with
+    | Tick | Drop -> moveShape (0, 1) model
+    | Left -> moveShape (-1, 0) model
+    | Right -> moveShape (1, 0) model
+    | Rotate -> rotateShape model
--- samples/tetris-clone/Program.fs
+++ samples/tetris-clone/Program.fs
@@ -2,120 +2,13 @@
 open System
 open Xelmish.Model
 
-type Model =
-| StartPage
-| Playing of PlayingState
-| GameOver of score:int
-| Quit
-and PlayingState = {
-    grid: Colour[,]
-    block: (int * int) * Shape * int
-    score: int
-}
-and Shape = {
-    positions: ((int * int) list) []
-    colour: Colour
-}
-
-let shapes = [
-    {
-        positions = 
-            [|
-                [0,0; 1,0; 0,1; 1,1]
-            |]
-        colour = Colours.cyan
-    }
-    {
-        positions = 
-            [|
-                [0,0; 1,0; 2,0; 3,0]
-                [2,0; 2,1; 2,2; 2,3]
-            |]
-        colour = Colours.red
-    }
-    {
-        positions =
-            [|
-                [0,0; 1,0; 1,1; 2,1]
-                [2,0; 2,1; 1,1; 1,2]
-            |] 
-        colour = Colours.green
-    }
-    {
-        positions =
-            [|
-                [1,0; 2,0; 1,1; 0,1]
-                [1,0; 1,1; 2,1; 2,2]
-            |] 
-        colour = Colours.blue
-    }
-    {
-        positions =
-            [|
-                [0,0; 1,0; 2,0; 0,1]
-                [1,0; 1,1; 1,2; 2,2]
-            |] 
-        colour = Colours.yellow
-    }
-    {
-        positions =
-            [|
-                [0,0; 1,0; 2,0; 2,1]
-                [1,0; 1,1; 1,2; 2,0]
-            |] 
-        colour = Colours.magenta
-    }
-    {
-        positions = 
-            [|
-                [0,0; 1,0; 2,0; 1,1]
-                [1,0; 1,1; 1,2; 0,1]
-                [0,1; 1,1; 2,1; 1,0]
-                [1,0; 1,1; 1,2; 2,1]
-            |]
-        colour = Colours.magenta
-    }
-]
-
-type Message = 
-| Tick
-| Drop
-| Left
-| Right
-| Rotate
-
-let tilesFor ((x, y), shape, rotationIndex) =
-    shape.positions.[rotationIndex] 
-    |> List.map (fun (dx, dy) -> x + dx, y + dy)
-
-let moveShape (dx, dy) model =
-    match model with
-    | Playing state ->
-        // change position
-        // calculate tiles
-        // if out of bounds, ignore
-        // if overlap, add prior position to grid and spawn new
-        // else set new position
-        model
-    | _ -> model
-
-let rotateShape model =
-    match model with
-    | Playing state ->
-        // change rotationIndex
-        // calculate tiles
-        // if out of bounds, ignore
-        // if overlap, add prior position to grid and spawn new
-        // else set new rotation
-        model
-    | _ -> model
-
-let update message model =
-    match message with
-    | Tick | Drop -> moveShape (0, 1) model
-    | Left -> moveShape (-1, 0) model
-    | Right -> moveShape (1, 0) model
-    | Rotate -> rotateShape model
+type Model = {
+    state: State
+} and State =
+    | StartPage
+    | Playing of Playing.Model
+    | GameOver of score:int
+    | Quit
 
 [<EntryPoint>]
 let main argv =
