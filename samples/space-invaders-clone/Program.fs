1f467b63bb9e2f4cd5456628775b4edea4749c13
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-04-14 08:27:24 +1200

    initial scaffolding for next sample

---
+++ samples/space-invaders-clone/Program.fs
@@ -0,0 +1,17 @@
+﻿
+type Model = {
+    playerX: int
+    invaders: (int * int) list
+    projectiles: (int * int * int) list
+}
+
+let init () = {
+    playerX = 0
+    invaders = []
+    projectiles = []
+}
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0
---
+++ samples/space-invaders-clone/readme.md
@@ -0,0 +1,1 @@
+﻿# Space Invaders Clone
