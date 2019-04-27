0b75cdc72a23901481f0dbe23e24bf5488cefebc
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-04-28 08:47:24 +1200

    interim work splitting out projectiles, and adding more messages

--- samples/space-invaders-clone/Program.fs
+++ samples/space-invaders-clone/Program.fs
@@ -5,9 +5,10 @@ open Config
 
 type PlayingModel = {
     playerX: int
+    playerProjectile: Projectile option
     invaders: Row []
     invaderDirection: ShuffleState
-    projectiles: Projectile list
+    invaderProjectiles: Projectile list
     lastShuffle: int64
     shuffleInterval: int64
     shuffleMod: int
@@ -15,11 +16,12 @@ type PlayingModel = {
 } 
 and Row = { kind: InvaderKind; y: int; xs: int [] }
 and ShuffleState = Across of row:int * dir:int | Down of row:int * nextDir:int
-and Projectile = { x: int; y: int; velocity: int }
+and Projectile = { x: int; y: int }
 
 let init () = 
     {
         playerX = resWidth / 2 - (playerWidth / 2)
+        playerProjectile = None
         invaders = 
             [|0..invaderRows-1|]
             |> Array.map (fun row -> 
@@ -33,7 +35,7 @@ let init () =
                             padding + col * (largeSize.width + invaderSpacing) + kind.offset) 
                 })
         invaderDirection = Across (invaderRows - 1, 1)
-        projectiles = []
+        invaderProjectiles = []
         lastShuffle = 0L
         shuffleInterval = 500L
         shuffleMod = 0
@@ -42,9 +44,11 @@ let init () =
 
 type Message = 
     | MovePlayer of dir: int
-    | FireProjectile of Projectile
+    | PlayerShoot
+    | InvaderShoot of x: int * y: int
     | ShuffleInvaders of int64
     | MoveProjectiles
+    | InvaderHit of row: int * index: int
     | PlayerHit
     | Victory
 
@@ -93,55 +97,63 @@ let rec shuffleInvaders time model =
             lastShuffle = time }, command
 
 let moveProjectiles model =
-    let playerProjectile (acc, playerHit, invadersHit) (projectile: Projectile) =
-        let next = { projectile with y = projectile.y + projectile.velocity }
-        if next.y < 0 then acc, false, invadersHit
-        else
-            //let projectileRect = rect next.x next.y 1 projectileHeight
-            //let hitInvaders = 
-            //    model.invaders 
-            //    |> List.filter (fun (ix, iy, iw, ih, _) -> 
-            //        projectileRect.Intersects(rect ix iy iw ih))
-            //if hitInvaders <> [] then
-            //    acc, playerHit, hitInvaders @ invadersHit
-            //else
-            next::acc, playerHit, invadersHit
+    model, Cmd.none
+//    let playerProjectile (acc, playerHit, invadersHit) (projectile: Projectile) =
+//        let next = { projectile with y = projectile.y + projectile.velocity }
+//        if next.y < 0 then acc, false, invadersHit
+//        else
+//            //let projectileRect = rect next.x next.y 1 projectileHeight
+//            //let hitInvaders = 
+//            //    model.invaders 
+//            //    |> List.filter (fun (ix, iy, iw, ih, _) -> 
+//            //        projectileRect.Intersects(rect ix iy iw ih))
+//            //if hitInvaders <> [] then
+//            //    acc, playerHit, hitInvaders @ invadersHit
+//            //else
+//            next::acc, playerHit, invadersHit
 
-    let invaderProjectile (acc, playerHit, invadersHit) (projectile: Projectile) =
-        let next = { projectile with y = projectile.y + projectile.velocity }
-        if next.y > resHeight then acc, playerHit, invadersHit
-        else
-            let overlapsPlayer = 
-                projectile.x >= model.playerX && projectile.x < model.playerX + playerWidth
-                && next.y >= playerY
-            if overlapsPlayer then acc, true, invadersHit
-            else next::acc, playerHit, invadersHit
+//    let invaderProjectile (acc, playerHit, invadersHit) (projectile: Projectile) =
+//        let next = { projectile with y = projectile.y + projectile.velocity }
+//        if next.y > resHeight then acc, playerHit, invadersHit
+//        else
+//            let overlapsPlayer = 
+//                projectile.x >= model.playerX && projectile.x < model.playerX + playerWidth
+//                && next.y >= playerY
+//            if overlapsPlayer then acc, true, invadersHit
+//            else next::acc, playerHit, invadersHit
 
-    let newProjectiles, playerHit, invadersHit =
-        (([], false, []), model.projectiles)
-        ||> List.fold (fun (acc, playerHit, invadersHit) projectile ->
-            if projectile.velocity > 0 then 
-                invaderProjectile (acc, playerHit, invadersHit) projectile
-            else 
-                playerProjectile (acc, playerHit, invadersHit) projectile)
+//    let newProjectiles, playerHit, invadersHit =
+//        (([], false, []), model.projectiles)
+//        ||> List.fold (fun (acc, playerHit, invadersHit) projectile ->
+//            if projectile.velocity > 0 then 
+//                invaderProjectile (acc, playerHit, invadersHit) projectile
+//            else 
+//                playerProjectile (acc, playerHit, invadersHit) projectile)
             
-    //let newInvaders = List.except invadersHit model.invaders
-    let command = 
-        if playerHit then Cmd.ofMsg PlayerHit 
-        //elif newInvaders = [] then Cmd.ofMsg Victory 
-        else Cmd.none
-    //{ model with projectiles = newProjectiles; invaders = newInvaders }, command
-    { model with projectiles = newProjectiles }, command
+//    //let newInvaders = List.except invadersHit model.invaders
+//    let command = 
+//        if playerHit then Cmd.ofMsg PlayerHit 
+//        //elif newInvaders = [] then Cmd.ofMsg Victory 
+//        else Cmd.none
+//    //{ model with projectiles = newProjectiles; invaders = newInvaders }, command
+//    { model with projectiles = newProjectiles }, command
 
 let update message model =
     match message with
     | MovePlayer dir ->
         let newPos = min (resWidth - padding - playerWidth) (max padding (model.playerX + dir * playerSpeed))
         { model with playerX = newPos }, Cmd.none
-    | FireProjectile projectile ->
-        { model with projectiles = projectile::model.projectiles }, Cmd.none
+    | PlayerShoot ->
+        let newProjectile = 
+            {   x = model.playerX + playerWidth / 2
+                y = resHeight - (playerHeight + padding) - projectileHeight - 1 }
+        { model with playerProjectile = Some newProjectile }, Cmd.none
+    | InvaderShoot (x, y) ->
+        let newProjectiles = { x = x; y = y }::model.invaderProjectiles
+        { model with invaderProjectiles = newProjectiles }, Cmd.none
     | ShuffleInvaders time -> shuffleInvaders time model        
     | MoveProjectiles -> moveProjectiles model
+    | InvaderHit (row, index) -> model, Cmd.none // TODO
     | PlayerHit -> { model with freeze = true }, Cmd.none
     | Victory -> { model with freeze = true }, Cmd.none
     
@@ -159,9 +171,15 @@ let view model dispatch =
 
         yield sprite spritemap.["player"] (playerWidth, playerHeight) (model.playerX, playerY) Colour.White
 
-        yield! model.projectiles
+        yield! model.invaderProjectiles
             |> List.map (fun projectile ->
                 colour Colour.White (1, projectileHeight) (projectile.x, projectile.y))
+
+        match model.playerProjectile with
+            | Some p -> 
+                yield colour Colour.White (1, projectileHeight) (p.x, p.y)
+            | _ -> 
+                yield onkeydown Keys.Space (fun () -> dispatch PlayerShoot)
 
         if not model.freeze then
             yield fun _ inputs _ -> 
@@ -174,15 +192,6 @@ let view model dispatch =
             yield whilekeydown Keys.A (fun () -> dispatch (MovePlayer -1))
             yield whilekeydown Keys.Right (fun () -> dispatch (MovePlayer 1))
             yield whilekeydown Keys.D (fun () -> dispatch (MovePlayer 1))
-
-        yield onkeydown Keys.Space (fun () -> 
-            // check that the player hasn't already fired a projectile before adding a new one
-            if not (model.projectiles |> List.exists (fun projectile -> projectile.velocity < 0)) then
-                {
-                    x = model.playerX + playerWidth / 2
-                    y = resHeight - (playerHeight + padding) - projectileHeight - 1
-                    velocity = -projectileSpeed
-                } |> FireProjectile |> dispatch)
 
         yield onkeydown Keys.Escape exit
     ]
