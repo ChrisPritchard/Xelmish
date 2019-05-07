d6d4f11fadc3e2a17119fe659533a4785e5d8027
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-05-07 13:06:11 +1200

    Comments

--- src/Viewables.fs
+++ src/Viewables.fs
@@ -1,4 +1,6 @@
-﻿module Xelmish.Viewables
+﻿/// This module contains functions that provide some stock viewables for use.
+ /// For example, drawing a rect of a given colour is defined here.
+ module Xelmish.Viewables
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
@@ -12,14 +14,19 @@ type KeyQueue = System.Collections.Generic.Queue<string>
 let private vector2 x y = Vector2(float32 x, float32 y)
 let private isInside tx ty tw th x y = x >= tx && x <= tx + tw && y >= ty && y <= ty + th
 
+/// Draw a coloured rect
 let colour colour (width, height) (x, y) = 
     OnDraw (fun loadedAssets spriteBatch -> 
         spriteBatch.Draw(loadedAssets.whiteTexture, rect x y width height, colour))
 
+/// Draw the image specified by the given key
 let image key colour (width, height) (x, y) = 
     OnDraw (fun loadedAssets spriteBatch -> 
         spriteBatch.Draw(loadedAssets.textures.[key], rect x y width height, colour))
 
+/// Draw text at the given size, colour and position. 
+/// The ox, oy origin vars determine where the text should be placed relative to the given position,
+/// e.g. 0., 0. would be right down from x, y whereas -1., 0. would be left, down, with the last character just before x.
 let text font (fontSize: float) colour (ox: float, oy: float) (text: string) (x, y) =
     OnDraw (fun loadedAssets spriteBatch -> 
         let font = loadedAssets.fonts.[font]
@@ -29,19 +36,18 @@ let text font (fontSize: float) colour (ox: float, oy: float) (text: string) (x,
         let position = Vector2.Add(origin, vector2 x y)
         spriteBatch.DrawString (font, text, position, colour, 0.f, Vector2.Zero, scale, SpriteEffects.None, 0.f))
 
-let playSound key =
-    OnDraw (fun loadedAssets _ -> ignore (loadedAssets.sounds.[key].Play ()))
-
+/// Play the next sound in a sound queue (a Queue<string> containing keys of sound effects to play)
 let playQueuedSound (soundQueue: KeyQueue) =
     OnDraw (fun loadedAssets _ ->
         if soundQueue.Count > 0 then 
             let nextSound = soundQueue.Dequeue ()
             ignore (loadedAssets.sounds.[nextSound].Play ()))
     
-
+/// Run the given event function on every call to Update by the game loop (approx 60 times a second)
 let onupdate event = 
     OnUpdate event
 
+/// Run the given event if the left mouse button has just been pressed in the specified area
 let onclick event (width, height) (x, y) =
     onupdate (fun inputs -> 
         if (inputs.mouseState.X, inputs.mouseState.Y) ||> isInside x y width height then
@@ -49,18 +55,21 @@ let onclick event (width, height) (x, y) =
             && inputs.lastMouseState.LeftButton <> ButtonState.Pressed then
                 event ())
 
+/// Run the given event if the given key has just been pressed
 let onkeydown key event =
     onupdate (fun inputs -> 
         if inputs.keyboardState.IsKeyDown key 
         && not (inputs.lastKeyboardState.IsKeyDown key) then 
             event ())
 
+/// Run the given event if the given key is currently pressed
 let whilekeydown key event =
     onupdate (fun inputs -> 
         if inputs.keyboardState.IsKeyDown key 
         && inputs.lastKeyboardState.IsKeyDown key then 
             event ())
 
+/// Run the given event if the given key has just been released
 let onkeyup key event  =
     onupdate (fun inputs -> 
         if not (inputs.keyboardState.IsKeyDown key)
