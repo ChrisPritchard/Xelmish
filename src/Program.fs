40498e051f660287ccb92ddf0b622566d499e517
Christopher Pritchard <chrispritchard.nz@gmail.com>
2019-05-07 12:49:28 +1200

    Comments

--- src/Program.fs
+++ src/Program.fs
@@ -5,6 +5,8 @@ open Elmish
 open GameLoop
 open Model
 
+/// Entry point to run an Elmish application using Xelmish.
+/// Expects a GameConfig variable with basic game loop setup config (e.g. resolution, assets)
 let runGameLoop config (program: Program<_, _, _, Viewable list>) =
     use loop = new GameLoop (config)
     let setState model dispatch =
@@ -12,6 +14,8 @@ let runGameLoop config (program: Program<_, _, _, Viewable list>) =
     Program.run { program with setState = setState }
     loop.Run ()
 
+/// Alternative entry point to Xelmish from Elmish.
+/// Accepts variables to configure common config properties for the game loop
 let runSimpleGameLoop assetsToLoad (windowWidth, windowHeight) clearColour (program: Program<_, _, _, Viewable list>) =
     let config = {
         resolution = Windowed (windowWidth, windowHeight)
