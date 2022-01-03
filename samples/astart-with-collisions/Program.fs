3fb220442b19fb83dc95fd50a5b1dbed20c732e3
Ricky <Ricky.y.c@outlook.com>
2022-01-03 17:27:51 +0800

    blank project

--- samples/astart-with-collisions/Program.fs
+++ samples/astart-with-collisions/Program.fs
@@ -2,33 +2,27 @@
 open Xelmish.Model 
 open Xelmish.Viewables 
 
-type Model = { x: int; y: int; w: int; h: int } 
+type Model = 
+    { 
+        T: int 
+    } 
 
-let init () = { x = 250; y = 250; w = 100; h = 100 } 
+let init () = 
+    { 
+        T = 0
+    }, Cmd.none
 
 type Message =
-    | MoveVertical of dir: int
-    | MoveHorizontal of dir: int
-    | Resize of dir: int
+    | E
 
 let update message model =
     match message with
-    | MoveVertical dir -> { model with y = model.y + 2 * dir }
-    | MoveHorizontal dir -> { model with x = model.x + 2 * dir } 
-    | Resize dir -> { x = model.x - dir; y = model.y - dir; w = model.w + 2 * dir; h = model.h + 2 * dir }
+    | E -> model, Cmd.none
 
 let view model dispatch =
     [
-        colour Colour.Aqua (model.w, model.h) (model.x, model.y)
-        whilekeydown Keys.Up (fun _ -> dispatch (MoveVertical -1))
-        whilekeydown Keys.Down (fun _ -> dispatch (MoveVertical 1))
-        whilekeydown Keys.Left (fun _ -> dispatch (MoveHorizontal -1))
-        whilekeydown Keys.Right (fun _ -> dispatch (MoveHorizontal 1))
-
-        whilekeydown Keys.OemPlus (fun _ -> dispatch (Resize 1))
-        whilekeydown Keys.OemMinus (fun _ -> dispatch (Resize -1))
         onkeydown Keys.Escape exit
     ]
 
-Program.mkSimple init update view
+Program.mkProgram init update view
 |> Xelmish.Program.runSimpleGameLoop [] (600, 600) Colour.Black
