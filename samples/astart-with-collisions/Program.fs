4672577eb640a0e53d03522b9404feaa0fb7c16f
Ricky <Ricky.y.c@outlook.com>
2022-01-04 22:36:43 +0800

    path finding update

--- samples/astart-with-collisions/Program.fs
+++ samples/astart-with-collisions/Program.fs
@@ -4,6 +4,7 @@ open Xelmish.Model
 open Xelmish.Viewables
 open Tiles
 open Collisions
+open AStar
 
 type Model =
     { tileLayer: tileLayer
@@ -50,11 +51,35 @@ let update msg model =
         let (newPlayer, newMsg) = Player.update pmsg model.player
         { model with player = newPlayer }, Cmd.map SubMsg newMsg
 
+let gcost _ _ = 1. 
+let fcost (x, y) (gx, gy) = 
+    (abs (gx - x) + abs (gy - y)) |> float 
+
+let neighbors model (x, y) = 
+    let (tcols, trows) = model.tileLayer.cols, model.tileLayer.rows
+    let ns = 
+        [ (x - 1, y)
+          (x, y - 1)
+          (x + 1, y)
+          (x, y + 1) ]
+        |> Seq.filter(fun (x, y) -> 
+            let id = TileLayer.getIndexByXY model.tileLayer (x, y)
+            x >= 0 && x < tcols && 
+            y >= 0 && y < trows && 
+            model.tileLayer.tiles.[id] <> 0)
+    in ns 
+
+let astarConfig model = 
+    { neighbours = neighbors model
+      gCost = gcost
+      fCost = fcost
+      maxIterations = None }
+
 let view model dispatch =
     [ TileLayer.renderTileLayerColor
         (function
-        | 0 -> Colour.Transparent
-        | _ -> Colour.Aqua)
+        | 0 -> Colour.White
+        | _ -> Colour.DarkGreen)
         model.tileLayer
 
       yield! Player.view model.player (SubMsg >> dispatch) model.collisions
@@ -72,13 +97,32 @@ let view model dispatch =
               * model.tileLayer.tileWidth
               + model.tileLayer.y
 
-          sb.Draw(ast.whiteTexture, Rectangle(x, y, model.tileLayer.tileWidth, model.tileLayer.tileHeight), Colour.Red))
+          sb.Draw(ast.whiteTexture, 
+                    Rectangle(x, y, model.tileLayer.tileWidth, model.tileLayer.tileHeight), 
+                    Colour.Red))
       
       // draw path. 
       OnDraw(fun ast inps sb -> 
-      ())
+        let start = 
+            model.player.pivot 
+            |> TileLayer.getTileXYbyAbsoluteXY model.tileLayer
+        let goal = 
+            (inps.mouseState.X, inps.mouseState.Y)
+            |> TileLayer.getTileXYbyAbsoluteXY model.tileLayer
+        
+        let search = model |> astarConfig |> search
+        match search start goal with 
+        | Some s -> 
+            s |> Seq.iter (fun (x, y) -> 
+                let (ax, ay) = 
+                    x * model.tileLayer.tileWidth + model.tileLayer.x, 
+                    y * model.tileLayer.tileHeight + model.tileLayer.y
+                sb.Draw(ast.whiteTexture, 
+                        Rectangle(ax, ay, model.tileLayer.tileWidth, model.tileLayer.tileHeight), 
+                        Colour.MonoGameOrange * 0.5f))
+        | None -> ())
 
       onkeydown Keys.Escape exit ]
 
 Program.mkProgram init update view
-|> Xelmish.Program.runSimpleGameLoop [] (1600, 900) Colour.Black
+|> Xelmish.Program.runSimpleGameLoop [] (1600, 900) Colour.White
